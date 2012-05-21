4dad8e388b1dbe290a9c50690c162f86aa552f65
marianosimone <marianosimone@gmail.com>
2012-05-21 11:41:49 -0300

    Moviendo todos los bugs a una sola version

--- static/js/bugs1.js
+++ static/js/bugs1.js
@@ -1,3 +1,6 @@
+// Guardando solo 3 highscores
+snake._max_scores = 3;
+
 // Eliminando el nivel 6
 snake._levels.pop()
 
@@ -9,11 +12,19 @@ snake._levels[2] = level_2;
 // Haciendo que la roja no te alargue
 snake._meal_options.o.reactions = [ function() {snake._increase_points(100);}, function() { snake._generate_meal();}];
 
+// Haciendo que la negra no haga nada
+snake._meal_options.t.reactions = [];
+
+// No mostrando el puntaje en pantalla
+snake._increase_points = function(quantity) {
+        snake._score += quantity;
+}
+
+// Matando a la lumbri antes de llegar a los bordes
 snake.old_collisions = snake.checkCollisions;
 var box_limit = snake._box_size + 2*snake._box_padding;
 snake.checkCollisions = function() {
     var head = snake._snake[0];
-    console.log(head.y + " vs " + snake._canvas.height);
     if ((head.x == snake._canvas.width-2*box_limit) || (head.x == box_limit) || (head.y == box_limit) || (head.y == snake._canvas.height-2*box_limit)) {
         snake._collision = true;
         return false;
--- static/js/bugs2.js
+++
@@ -1,8 +0,0 @@
-snake._max_scores = 3;
-
-// Haciendo que la negra no haga nada
-snake._meal_options.t.reactions = [];
-
-snake._increase_points = function(quantity) {
-        snake._score += quantity;
-}
