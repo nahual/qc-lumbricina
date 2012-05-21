585119b54ed340cc90e2ab2a3fea3a7d9e14a22c
Mariano Simone <marianosimone@gmail.com>
2012-05-20 22:12:42 -0300

    BUGS lumbricina muere antes de llegar a los bordes

--- static/js/bugs1.js
+++ static/js/bugs1.js
@@ -8,3 +8,16 @@ snake._levels[2] = level_2;
 
 // Haciendo que la roja no te alargue
 snake._meal_options.o.reactions = [ function() {snake._increase_points(100);}, function() { snake._generate_meal();}];
+
+snake.old_collisions = snake.checkCollisions;
+var box_limit = snake._box_size + 2*snake._box_padding;
+snake.checkCollisions = function() {
+    var head = snake._snake[0];
+    console.log(head.y + " vs " + snake._canvas.height);
+    if ((head.x == snake._canvas.width-2*box_limit) || (head.x == box_limit) || (head.y == box_limit) || (head.y == snake._canvas.height-2*box_limit)) {
+        snake._collision = true;
+        return false;
+    }
+    return snake.old_collisions();
+};
+
