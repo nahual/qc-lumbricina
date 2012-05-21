a29a76a394ddac001c5544b2d85bbe22966261ac
Mariano Simone <marianosimone@gmail.com>
2012-05-20 21:57:15 -0300

    BUGS no mostrando los puntos en pantalla en la v2

--- static/js/bugs2.js
+++ static/js/bugs2.js
@@ -2,3 +2,7 @@ snake._max_scores = 3;
 
 // Haciendo que la negra no haga nada
 snake._meal_options.t.reactions = [];
+
+snake._increase_points = function(quantity) {
+        snake._score += quantity;
+}
