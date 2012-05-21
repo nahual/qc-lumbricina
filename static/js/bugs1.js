7eab95a2ebeaca2ad9ebf2fbca9c1d71b8c7a4ac
Mariano Simone <marianosimone@gmail.com>
2012-05-20 21:53:00 -0300

    BUGS haciendo que la pastilla roja no alargue a la lumbri en la v1

--- static/js/bugs1.js
+++ static/js/bugs1.js
@@ -5,3 +5,6 @@ snake._levels.pop()
 var level_2 = snake._levels[1];
 snake._levels[1] = snake._levels[2];
 snake._levels[2] = level_2;
+
+// Haciendo que la roja no te alargue
+snake._meal_options.o.reactions = [ function() {snake._increase_points(100);}, function() { snake._generate_meal();}];
